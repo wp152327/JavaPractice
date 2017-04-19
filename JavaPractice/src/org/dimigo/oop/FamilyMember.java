225a00d91c4f57ef802dd5036975b3567ddfdf3b
wp152327 <thisissimple2731@gmail.com>
2017-04-19 12:28:28 +0900

    Update FamilyMember.java

--- JavaPractice/src/org/dimigo/oop/FamilyMember.java
+++ JavaPractice/src/org/dimigo/oop/FamilyMember.java
@@ -1,2 +1,18 @@
-Public Class FamilyMember {
+package oop.dimigo.org
+
+public class FamilyMember {
+  private static int memberCnt = 0;
+  private String memberName;
   
+  public FamilyMember(String memberName) {
+    this.memberName = memberName;
+  }
+  
+  public void getMemberName(){
+    return memberName;
+  }
+  
+  public static void printMemberCnt() {
+    System.out.println("ㄱㅏㅈㅗㄱ ㅊㅗㅇ ㅇㅣㄴㅇㅜㅓㄴㅅㅜ : " + memberCnt + "ㅁㅕㅇ");
+  }
+}
