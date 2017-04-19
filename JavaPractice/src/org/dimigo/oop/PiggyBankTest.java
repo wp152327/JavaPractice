aecf5c9d3ef4fc3faca61229be76df087a92796d
wp152327 <thisissimple2731@gmail.com>
2017-04-19 12:48:21 +0900

    Update PiggyBankTest.java

--- JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -2,5 +2,18 @@ package oop.dimigo.org
 
 public class PigggyBankTest {
   public static void main(String[] args) {
+    FamilyMember fm1 = new FamilyMember("Dad");
+    FamilyMember fm2 = new FamilyMember("Mom");
+    FamilyMember fm3 = new FamilyMember("Me");
+    FamilyMember fm4 = new FamilyMember("Brother");
+    
+    FamilyMemmber.printMemberCnt();
+    
+    PiggyBank.putMoney(fm1, 10000);
+    PiggyBank.putMoney(fm2, 5000);
+    PiggyBank.putMoney(fm3, 2000);
+    PiggyBank.putMoney(fm4, 1000);
+    
+    PiggyBank.printBalance();
   }
 ]
