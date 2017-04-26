35e0c049b138120b5f254712e81591c0df6e2be8
wp152327 <thisissimple2731@gmail.com>
2017-04-26 12:41:39 +0900

    Update Person.java

--- JavaPractice/src/org/dimigo/inheritance/Person.java
+++ JavaPractice/src/org/dimigo/inheritance/Person.java
@@ -1,3 +1,31 @@
 package org.dimigo.inheritance
 
-public class Person
+public class Person {
+  private String name;
+  
+  public Person() {}
+  
+  public Person(String name) {
+    this.name = name;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public void sayHello() {
+    System.out.println("Hello!");
+  }
+  
+  public void sayBye() {
+    System.out.println("Bye!");
+  }
+  
+  public String toString() {
+    return "I'm " + name;
+  }
+}
