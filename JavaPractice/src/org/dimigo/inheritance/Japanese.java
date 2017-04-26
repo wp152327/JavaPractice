e8a282c6643ea9316a2554ed50faa9541e912ffc
wp152327 <thisissimple2731@gmail.com>
2017-04-26 12:46:12 +0900

    Create Japanese.java

---
+++ JavaPractice/src/org/dimigo/inheritance/Japanese.java
@@ -0,0 +1,20 @@
+
+package org.dimigo.inheritance
+
+public class Japanese extends Person {
+  public Japanese(String name) {
+    super(name);
+  }
+  
+  public void sayHello() {
+    System.out.println("Hello in Japanese!");
+  }
+  
+  public void sayBye() {
+    System.out.println("Bye in Japanese!");
+  }
+  
+  public String toString() {
+    return "I'm Japanese " + name;
+  }
+}
