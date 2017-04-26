11413c57222227f8bb03e7c482513d544bc7e19d
wp152327 <thisissimple2731@gmail.com>
2017-04-26 12:44:55 +0900

    Update Korean.java

--- JavaPractice/src/org/dimigo/inheritance/Korean.java
+++ JavaPractice/src/org/dimigo/inheritance/Korean.java
@@ -2,4 +2,18 @@ package org.dimigo.inheritance
 
 public class Korean extends Person {
   public Korean(String name) {
-    
+    super(name);
+  }
+  
+  public void sayHello() {
+    System.out.println("Hello in Korean!");
+  }
+  
+  public void sayBye() {
+    System.out.println("Bye in Korean!");
+  }
+  
+  public String toString() {
+    return "I'm Korean " + name;
+  }
+}
