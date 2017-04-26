c23705a5c4af95f2833af7195c2428e7bb2738d4
wp152327 <thisissimple2731@gmail.com>
2017-04-26 12:47:14 +0900

    Create Chinese.java

---
+++ JavaPractice/src/org/dimigo/inheritance/Chinese.java
@@ -0,0 +1,20 @@
+
+package org.dimigo.inheritance
+
+public class Chinese extends Person {
+  public Chinese(String name) {
+    super(name);
+  }
+  
+  public void sayHello() {
+    System.out.println("Hello in Chinese!");
+  }
+  
+  public void sayBye() {
+    System.out.println("Bye in Chinese!");
+  }
+  
+  public String toString() {
+    return "I'm Chinese " + name;
+  }
+}
