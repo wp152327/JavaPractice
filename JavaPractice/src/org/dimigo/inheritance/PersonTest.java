126caa76f7bc207064499aca9c9b6ea4e763cbb1
wp152327 <thisissimple2731@gmail.com>
2017-04-26 12:51:06 +0900

    Create PersonTest.java

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest.java
@@ -0,0 +1,9 @@
+package org.dimigo.inheritance
+
+public class PersonTest {
+  public static void main(String args[]) {
+    Person p = new Person("Tom");
+    System.out.println(p);
+    
+    Korean k = new Korean("JeonYunMin");
+    System.out.println(k);
